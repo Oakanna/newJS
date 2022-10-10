3d921ce35d20f2246ef7db9fdc5aad968a9f2275
Anna <example@gmail.com>
2022-10-10 13:43:10 +0300

    Lessen#4 added chooseIncome

--- script.js
+++ script.js
@@ -19,112 +19,89 @@ let appData = {
   optionalExpenses: {},
   income: [],
   savings: true,
-};
+  chooseExpenses: function () {
+    for (let i = 0; i < 2; i++) {
+      let a = prompt("Введите обязательную статью расходов в этом месяце", ""),
+        b = +prompt("Во сколько обойдется?", "");
 
-// * do...while
-// let i = 0;
-// do {
-//   let a = prompt("Введите обязательную статью расходов в этом месяце", ""),
-//     b = prompt("Во сколько обойдется?", "");
-
-//   if (
-//     typeof a === "string" &&
-//     (typeof a != null) & (typeof b != null) &&
-//     a != "" &&
-//     b != ""
-//   ) {
-//     console.log("done");
-//     appData.expenses[a] = b;
-//   }else {
-//     console.log("Bad girl");
-//     i--;
-//   }
-//   i++;
-// } while (i < 2);
-
-//* while
-// while (i < 2) {
-//   let a = prompt("Введите обязательную статью расходов в этом месяце", ""),
-//     b = prompt("Во сколько обойдется?", "");
-
-//   if (
-//     typeof a === "string" &&
-//     (typeof a != null) & (typeof b != null) &&
-//     a != "" &&
-//     b != ""
-//   ) {
-//     console.log("done");
-//     appData.expenses[a] = b;
-//   }else {
-//     console.log("Bad girl");
-//     i--;
-//   }
-//   i++;
-
-// }
-
-//* for
-function chooseExpenses() {
-  for (let i = 0; i < 2; i++) {
-    let a = prompt("Введите обязательную статью расходов в этом месяце", ""),
-      b = +prompt("Во сколько обойдется?", "");
-
-    if (
-      typeof a === "string" &&
-      (typeof a != null) & (typeof b != null) &&
-      a != "" &&
-      b != "" &&
-      a.length < 50
-    ) {
-      console.log("done");
-      appData.expenses[a] = b;
-    } else {
-      console.log("Bad girl");
-      i--;
+      if (
+        typeof a === "string" &&
+        (typeof a != null) & (typeof b != null) &&
+        a != "" &&
+        b != "" &&
+        a.length < 50
+      ) {
+        console.log("done");
+        appData.expenses[a] = b;
+      } else {
+        console.log("Bad girl");
+        i--;
+      }
     }
-  }
-}
+  },
+  detectDayBudget: function () {
+    appData.moneyPerDay = (appData.budget / 30).toFixed();
+    alert(`Бюджет за день: ${appData.moneyPerDay} rub`);
+  },
+  detectLevel: function () {
+    if (appData.moneyPerDay < 100) {
+      console.log("Поменяй работу, нищеброд");
+    } else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+      console.log("Not bad");
+    } else if (appData.moneyPerDay > 2000) {
+      console.log("Excellent");
+    } else {
+      console.log("WTF is this?");
+    }
+  },
+  checkSaving: function () {
+    if (appData.savings == true) {
+      let save = +prompt("Какова ваша сумма накоплений?"),
+        percent = +prompt("Под какой процент?");
 
+      appData.monthIncome = ((save / 100 / 12) * percent).toFixed();
+      alert(`Доход в месяц с вашего депозита: ${appData.monthIncome}`);
+    }
+  },
+  chooseOptExpenses: function () {
+    for (let i = 1; i < 4; i++) {
+      let option = prompt("Статья необязательных расходов?");
+
+      appData.optionalExpenses[i] = option;
+    }
+  },
+  chooseIncome: function () {
+    let item = prompt(
+      "Что принесет дополнительный доход? (Перечислите через запятуюю)"
+    );
+    if (typeof item !== "string" && typeof item == null && item == "") {
+      console.log("Вы ввели некорректные данные или не ввели их вовсе");
+    } else {
+      appData.income = item.split(", ");
+      appData.income.push(prompt("Может что-то еще?"));
+      appData.income.sort();
+    }
+
+    appData.income.forEach(function (item, index) {
+      alert("Способы доп. заработка: " + ++index + " - " + item);
+    });
+  },
+};
+for (let key in appData) {
+  console.log(
+    "Наша программа включает в себя данные: " + key + " - " + appData[key]
+  );
+}
+// 3) Используя цикл for in для объекта (appData) вывести в консоль сообщение "Наша программа включает в себя данные: " (вывести весь appData)
+// 4) Проверить, чтобы все работало и не было ошибок в консоли
+// 5) Добавить папку с уроком на свой GitHub
 // chooseExpenses();
 
-function detectDayBudget() {
-  appData.moneyPerDay = (appData.budget / 30).toFixed();
-  alert(`Бюджет за день: ${appData.moneyPerDay} rub`);
-}
 // detectDayBudget();
-
-function detectLevel() {
-  if (appData.moneyPerDay < 100) {
-    console.log("Поменяй работу, нищеброд");
-  } else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
-    console.log("Not bad");
-  } else if (appData.moneyPerDay > 2000) {
-    console.log("Excellent");
-  } else {
-    console.log("WTF is this?");
-  }
-}
 
 // detectLevel();
 
-function checkSaving() {
-  if (appData.savings == true) {
-    let save = +prompt("Какова ваша сумма накоплений?"),
-      percent = +prompt("Под какой процент?");
-
-    appData.monthIncome = ((save / 100 / 12) * percent).toFixed();
-    alert(`Доход в месяц с вашего депозита: ${appData.monthIncome}`);
-  }
-}
 // checkSaving();
 
-function chooseOptExpenses() {
-  for (let i = 1; i < 4; i++) {
-    let option = prompt("Статья необязательных расходов?");
-
-    appData.optionalExpenses[i] = option;
-  }
-}
-
-chooseOptExpenses();
+// chooseOptExpenses();
 console.log(appData);
